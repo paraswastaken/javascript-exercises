18cbb4e8cc38b4465c71577e07117ef033b5c2cc
paraswastaken <akarkaulia2000@gmail.com>
2022-06-04 23:17:18 +0530

    calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(a of arr){
+    sum = sum + a;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for(a of arr){
+    prod = prod * a;
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let out = 1;
+  while(b!=0){
+    out = out*a
+    b--;
+  }
+  return out;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let temp = 1;
+  while(a!=0){
+    temp=temp*a;
+    a--;
+  }
+  return temp;
 };
 
 // Do not edit below this line
