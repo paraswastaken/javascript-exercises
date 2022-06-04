683d10346218c6af987ee62f9037257d19f09aff
paraswastaken <akarkaulia2000@gmail.com>
2022-06-04 23:36:54 +0530

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let i, arr;
+    str = str.toLowerCase();
+    arr = Array.from(str);
+    arr = arr.filter((st)=>st>='a' && st<='z');
+    for(i=0;i<arr.length;i++){
+        if(arr[i]!=arr[arr.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
