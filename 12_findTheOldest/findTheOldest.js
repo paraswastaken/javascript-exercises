98aba3f7737e8b2b94498ae0a9461a15fce49dd9
paras <akarkaulia2000@gmail.com>
2022-06-13 19:47:32 +0530

    Solve 12th problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let out = people.reduce((oldest, obj)=>{
+        if(!("yearOfDeath" in oldest)){
+            oldestObj = (new Date().getFullYear()) - oldest.yearOfBirth;
+        }
+        else oldestObj = oldest.yearOfDeath - oldest.yearOfBirth;
+        if(!("yearOfDeath" in obj)){
+            currObj = (new Date().getFullYear()) - obj.yearOfBirth;
+        }
+        else currObj = obj.yearOfDeath - obj.yearOfBirth;
+        if(currObj > oldestObj){return obj}
+        return oldest;
+    }, people[0]);
+    return out;
 };
 
 // Do not edit below this line
